4be249b0b7c78ab3b26c543c153335ce7b435311
Brandon <Brandon@winona.edu>
2020-01-11 18:08:42 -0600

    init commit

---
+++ src/main/java/com/bsllc/HelloMaven2/App.java
@@ -0,0 +1,13 @@
+package com.bsllc.HelloMaven2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Maven2!" );
+    }
+}
